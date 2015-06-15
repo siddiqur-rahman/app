f28a5358b9c2e6bc1693e880d49c9260280f8ffa
Siddiqur Rahman <siddiqur.rahman077@gmail.com>
2015-06-16 04:57:02 +0600

    no message

---
+++ qa/index.php
@@ -0,0 +1,12 @@
+<?php
+//take the question provided in get method 
+if (isset($_GET['q'])){
+  $query = $_GET['q'];
+  $query=rawurldecode($query);
+  echo $query;  
+}
+
+$url='https://www.google.com.bd/search?q='.$query;	
+$result = file_get_contents($url);
+echo $result;
+?>
