d7fbddf1c9fd5c74c554c8f7dc53defe12b76ebf
Siddiqur Rahman <siddiqur.rahman077@gmail.com>
2015-06-16 07:59:23 +0600

    no message

--- qa/index.php
+++ qa/index.php
@@ -10,7 +10,8 @@ $ans['answer']='Your majesty! Jon Snow knows nothing! So do I!';
 
 $url='https://www.google.com/search?q='.$query;	
 $result = file_get_contents($url);
-$narrow=stristr($result,'"_m3b>"');
+
+$narrow=stristr($result,'_m3b');
 echo $narrow; 
 echo "<br>";
 if($narrow!=false){
