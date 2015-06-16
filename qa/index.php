357e51167e684738660867bfc61c253d53f1090a
Siddiqur Rahman <siddiqur.rahman077@gmail.com>
2015-06-16 07:46:28 +0600

    no message

--- qa/index.php
+++ qa/index.php
@@ -6,8 +6,15 @@ if (isset($_GET['q'])){
   $query=rawurlencode($query);
 }
 
+$ans['answer']='Your majesty! Jon Snow knows nothing! So do I!';
+
 $url='https://www.google.com/search?q='.$query;	
 $result = file_get_contents($url);
-echo $result;
-echo $query; echo "<br> <br>" ;
+$narrow=stristr($query,"_m3b>");
+if($narrow!=false){
+	$finalAns=$query=stristr($query,"<",true);
+	$ans['answer'] = $finalAns;
+}
+header('Content-type: application/json');
+echo json_encode($ans);
 ?>
